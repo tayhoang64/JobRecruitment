fc0b2b05052fd07cd684d2c0715d2e88e895ceda
UocDuocTTTMA <ngtrkien1105@gmail.com>
2024-12-03 13:24:06 +0700

    ditconmecuocdoi

--- Controllers/CertificatesController.cs
+++ Controllers/CertificatesController.cs
@@ -1,9 +1,10 @@
 ﻿using CVRecruitment.Models;
 using CVRecruitment.Services;
-using Microsoft.AspNetCore.Http;
+using CVRecruitment.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CVRecruitment.Controllers
 {
@@ -13,110 +14,117 @@ namespace CVRecruitment.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly CvrecruitmentContext _context;
-        private readonly CloudinaryService _cloudinaryService;
+        private readonly FileService _fileService;
         private readonly UserManager<User> _userManager;
 
-        public CertificatesController(IConfiguration configuration, CvrecruitmentContext context, CloudinaryService cloudinaryService, UserManager<User> userManager)
+        public CertificatesController(IConfiguration configuration, CvrecruitmentContext context, FileService fileService, UserManager<User> userManager)
         {
             _configuration = configuration;
             _context = context;
-            _cloudinaryService = cloudinaryService;
+            _fileService = fileService;
             _userManager = userManager;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateCertificate([FromBody] Certificate certificate)
+        private async Task<(Models.User user, IActionResult result)> CheckLogin()
         {
-            if (certificate == null)
+            var user = (Models.User)HttpContext.Items["User"];
+            if (user == null)
             {
-                return BadRequest("Certificate data is required.");
+                return (null, Unauthorized(new { message = "Invalid token" }));
+            }
+            return (user, null);
+        }
+
+
+        // Create a certificate
+        [HttpPost]
+        public async Task<IActionResult> CreateCertificate(ViewModels.CertificatesViewModel certificate)
+        {
+            var (user, result) = await CheckLogin();
+            if(result != null)
+            {
+                return result;
             }
 
-            await _context.Certificates.AddAsync(certificate);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Certificate newCertificate = new Certificate()
+            {
+                CertificateName = certificate.CertificateName,
+                Organization = certificate.Organization,
+                IssueMonth = certificate.IssueMonth,
+                IssueYear = certificate.IssueYear,
+                CertificateUrl = certificate.CertificateUrl,
+                Description = certificate.Description,
+                UserId = user.Id,
+            };
+
+            _context.Certificates.Add(newCertificate);
             await _context.SaveChangesAsync();
-            return Ok(new { Message = "Certificate created successfully!" });
+
+            return Ok(newCertificate);
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetCertificates()
+        public async Task<IActionResult> GetMyCertificates()
         {
-            var certificates = await _context.Certificates.ToListAsync();
-            return Ok(certificates);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetCertificateById(int id)
-        {
-            var certificate = await _context.Certificates.FindAsync(id);
-            if (certificate == null)
+            var (user, result) = await CheckLogin();
+            if (result != null)
             {
-                return NotFound("Certificate not found.");
+                return result;
             }
-            return Ok(certificate);
-        }
+            return Ok(_context.Certificates.Where(c=>c.UserId==user.Id));
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCertificate(int id, [FromBody] Certificate updatedCertificate)
-        {
-            var certificate = await _context.Certificates.FindAsync(id);
-            if (certificate == null)
-            {
-                return NotFound("Certificate not found.");
-            }
-
-            // Cập nhật thông tin
-            certificate.CertificateName = updatedCertificate.CertificateName;
-            certificate.Organization = updatedCertificate.Organization;
-            certificate.IssueMonth = updatedCertificate.IssueMonth;
-            certificate.IssueYear = updatedCertificate.IssueYear;
-            certificate.CertificateUrl = updatedCertificate.CertificateUrl;
-            certificate.Description = updatedCertificate.Description;
-            certificate.UserId = updatedCertificate.UserId;
-
-            await _context.SaveChangesAsync();
-            return Ok(new { Message = "Certificate updated successfully!" });
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCertificate(int id)
+        public async Task<IActionResult> DeleteCertificates(int id)
         {
-            var certificate = await _context.Certificates.FindAsync(id);
-            if (certificate == null)
+            var (user, result) = await CheckLogin();
+            if (result != null)
             {
-                return NotFound("Certificate not found.");
+                return result;
             }
-
+            var certificate = _context.Certificates.FirstOrDefault(certificate => certificate.CertificateId ==id);
+            if (certificate == null) {
+                return NotFound("Certificates not found");
+            }
+            if (certificate.UserId != user.Id) {
+                return Forbid();
+            }
             _context.Certificates.Remove(certificate);
             await _context.SaveChangesAsync();
-            return Ok(new { Message = "Certificate deleted successfully!" });
+            return Ok("Finish!!");
         }
 
-        //[HttpPost("upload")]
-        //public async Task<IActionResult> UploadCertificate([FromForm] IFormFile file, int userId)
-        //{
-        //    if (file == null || file.Length == 0)
-        //    {
-        //        return BadRequest("File is required.");
-        //    }
-
-        //    var uploadResult = await _cloudinaryService.UploadFileAsync(file);
-        //    if (uploadResult == null)
-        //    {
-        //        return StatusCode(500, "Error uploading file.");
-        //    }
-
-        //    var certificate = new Certificate
-        //    {
-        //        CertificateName = file.FileName,
-        //        CertificateUrl = uploadResult.Url,
-        //        UserId = userId
-        //    };
-
-        //    await _context.Certificates.AddAsync(certificate);
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok(new { Message = "Certificate uploaded successfully!", Certificate = certificate });
-        //}
-
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCertificates(int id, CertificatesViewModel UpdateCertificates)
+        {
+            var (user, result) = await CheckLogin();
+            if (result != null)
+            {
+                return result;
+            }
+            var certificate = _context.Certificates.FirstOrDefault(certificate => certificate.CertificateId == id);
+            if (certificate == null)
+            {
+                return NotFound("Certificates not found");
+            }
+            if (certificate.UserId != user.Id)
+            {
+                return Forbid();
+            }
+            certificate.CertificateName = UpdateCertificates.CertificateName;
+            certificate.Organization = UpdateCertificates.Organization;
+            certificate.IssueMonth = UpdateCertificates.IssueMonth;
+            certificate.IssueYear = UpdateCertificates.IssueYear;
+            certificate.CertificateUrl = UpdateCertificates.CertificateUrl;
+            certificate.Description = UpdateCertificates.Description;
+            await _context.SaveChangesAsync();
+            return Ok("Updated succesfully");
+        }
     }
 }
